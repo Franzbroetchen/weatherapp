e3b7ba72f207490d9a2ad0a5b0fae3e2793caf05
Franzbroetchen <franziska.probst@xintec.de>
2021-01-30 12:38:33 +0100

    project start

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Suche..."/>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      api_key: '98a1c4d5efaf512acd6bb740307587bc'
+    }
+  }
+}
+</script>
+
+<style>
+
+* {
+  margin: 0;
+  padding:0;
+  box-sizing:border-box;
+}
+
+body{
+  font-family: 'montserrat', sans sans-serif;
+}
+
+#app {
+  background-image:url('./assets/sunset.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main{
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom,rgba(0,0,0,0.25),rgba(0,0,0,0.75));
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(255, 255, 255,0.25);
+  background-color: rgba(255, 255, 255,0.75);
+}
+</style>
